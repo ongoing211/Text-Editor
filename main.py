c46f36beb6d102c845418aba9820fb5f41b71fde
ongoing211 <56183731+ongoing211@users.noreply.github.com>
2024-10-04 21:18:05 +0300

    Update main.py
    Новая кнопка

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ root.title('Текстовый редактор ongoing211')
 txt_edit = Text(root)
 txt_edit.grid()
 
+btn_open = Button(root, text='Открыть файл...')
 btn_save = Button(root, text='Сохранить как...')
-btn_save.place(y=200, x=50)
+btn_open.place(x=50, y=150)
+btn_save.place(x=50, y=200)
 
 root.mainloop()
