64f9a34269a02fe1b00367501f282bb17cc885cf
ongoing211 <56183731+ongoing211@users.noreply.github.com>
2024-10-07 15:17:39 +0300

    Update main.py
    Добавила frame

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from tkinter import *
 
 
+root = Tk("TextEditor")
 root.title('Текстовый редактор ongoing211')
 
-txt_edit = Text(root)
-txt_edit.grid()
+frame_btn = Frame(master=root, width=190, bg='grey')
+frame_btn.pack(fill=Y, side=LEFT)
 
-btn_open = Button(root, text='Открыть файл...')
-btn_save = Button(root, text='Сохранить как...')
+
+btn_open = Button(frame_btn, text='Открыть файл...')
+btn_save = Button(frame_btn, text='Сохранить как...')
 btn_open.place(x=50, y=150)
 btn_save.place(x=50, y=200)
 
