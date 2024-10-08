20655af318e43f668434283ec45a207496b7f7be
ongoing211 <56183731+ongoing211@users.noreply.github.com>
2024-10-08 15:38:38 +0300

    Update main.py
    Добавлен каркас новой функции

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ root = Tk("TextEditor")
 root.title('Текстовый редактор ongoing211')
 
 frame_btn = Frame(master=root, width=190, bg='grey')
+txt_edit = Frame(master=root)
 frame_btn.pack(fill=Y, side=LEFT)
 
 
@@ -12,5 +13,10 @@ btn_open = Button(frame_btn, text='Открыть файл...')
 btn_save = Button(frame_btn, text='Сохранить как...')
 btn_open.place(x=50, y=150)
 btn_save.place(x=50, y=200)
+
+def open_file():
+    file_path = fd.askopenfilename(filetypes=[("Text files", ".txt"), ("All files", "*.*")])
+    if not file_path:
+        return
 
 root.mainloop()
